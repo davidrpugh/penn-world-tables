90c49d90db2ee47cfb010df72f408eddde9f27ae
David R. Pugh <drobert.pugh@gmail.com>
2014-04-29 13:57:09 +0100

    Fixed a small typo!

--- pwt.py
+++ pwt.py
@@ -1,4 +1,6 @@
 from __future__ import division 
+import zipfile
+
 import requests
 
 # will need to check back regularly to see if base url has changed!
@@ -6,7 +8,7 @@ base_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/'
 
 def get_pwt_data(base_url):
     """Downloads the PWT data from the web."""
-    file_names = ['pwt.zip', 'depreciation_rates.zip']
+    file_names = ['pwt80.zip', 'depreciation_rates.zip']
     
     for file_name in file_names:
         tmp_buffer = requests.get(url=base_url + file_name)
