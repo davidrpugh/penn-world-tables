a01066461ee2dd0e83ffa4e0c5295b58e93c5cac
David R. Pugh <drobert.pugh@gmail.com>
2014-04-29 13:28:49 +0100

    Added code for grabbing the depreciation rates data.

--- pwt.py
+++ pwt.py
@@ -11,3 +11,11 @@ pwt_buffer = requests.get(url=pwt_url)
 with open('pwt80.zip', 'wb') as pwt_zip_file:
     pwt_zip_file.write(pwt_buffer.content)
 
+dep_rates_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/depreciation_rates.zip'
+
+# Connect to the depreciation rates data...
+dep_rates_buffer = requests.get(url=dep_rates_url)
+
+# ...save depreciation rates files to disk...
+with open('depreciation_rates.zip', 'wb') as dep_rates_zip_file:
+    dep_rates_zip_file.write(dep_rates_buffer.content)
