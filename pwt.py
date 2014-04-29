270b34f7c47c417bc6c2c05f781c577ccfcaa530
David R. Pugh <drobert.pugh@gmail.com>
2014-04-29 13:53:01 +0100

    Refactored two functions into a single function.

--- pwt.py
+++ pwt.py
@@ -6,26 +6,17 @@ base_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/'
 
 def get_pwt_data(base_url):
     """Downloads the PWT data from the web."""
-    file_name = 'pwt.zip'
-    pwt_buffer = requests.get(url=base_url + file_name)
+    file_names = ['pwt.zip', 'depreciation_rates.zip']
     
-    with open(file_name, 'wb') as pwt_zip_file:
-        pwt_zip_file.write(pwt_buffer.content)
-
-    print(file_name + ' successfully downloaded!')
+    for file_name in file_names:
+        tmp_buffer = requests.get(url=base_url + file_name)
     
-def get_dep_rates_data(base_url):
-    """Downloads the data on depreciation rates from the web."""
-    file_name = 'depreciation_rates.zip'
-    dep_rates_buffer = requests.get(url=base_url + file_name)
+        with open(file_name, 'wb') as tmp_zip_file:
+            tmp_zip_file.write(tmp_buffer.content)
 
-    with open('depreciation_rates.zip', 'wb') as dep_rates_zip_file:
-        dep_rates_zip_file.write(dep_rates_buffer.content)
-
-    print(file_name + ' successfully downloaded!')
-        
+        print(file_name + ' successfully downloaded!')
+            
 if __name__ == '__main__':
     base_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/'
 
     get_pwt_data(base_url)
-    get_dep_rates_data(base_url)
