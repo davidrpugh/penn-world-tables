30c661e33e35c6aacf21a1a427cf419309baaab1
David R. Pugh <drobert.pugh@gmail.com>
2014-04-29 13:50:09 +0100

    Added some success messages.

--- pwt.py
+++ pwt.py
@@ -6,20 +6,25 @@ base_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/'
 
 def get_pwt_data(base_url):
     """Downloads the PWT data from the web."""
-    pwt_buffer = requests.get(url=base_url + 'pwt.zip')
+    file_name = 'pwt.zip'
+    pwt_buffer = requests.get(url=base_url + file_name)
     
-    with open('pwt80.zip', 'wb') as pwt_zip_file:
+    with open(file_name, 'wb') as pwt_zip_file:
         pwt_zip_file.write(pwt_buffer.content)
 
+    print(file_name + ' successfully downloaded!')
+    
 def get_dep_rates_data(base_url):
     """Downloads the data on depreciation rates from the web."""
-    dep_rates_buffer = requests.get(url=base_url + 'depreciation_rates.zip')
+    file_name = 'depreciation_rates.zip'
+    dep_rates_buffer = requests.get(url=base_url + file_name)
 
     with open('depreciation_rates.zip', 'wb') as dep_rates_zip_file:
         dep_rates_zip_file.write(dep_rates_buffer.content)
+
+    print(file_name + ' successfully downloaded!')
         
 if __name__ == '__main__':
-    # will need to check back regularly to see if base url has changed!
     base_url = 'http://www.rug.nl/research/ggdc/data/pwt/v80/'
 
     get_pwt_data(base_url)
