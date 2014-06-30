9c5a6fe2fe07be6f0fd9550d30257d93fcece268
David R. Pugh <drobert.pugh@gmail.com>
2014-06-30 12:16:23 +0100

    Refactored functions.

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,12 @@
 import numpy as np
+from scipy import stats
+
 import pwt
 
 # load the Penn World Tables data
 pwt_data = pwt.load_pwt_data()
 
-def predicted_labor_share(capital, g, n, s, delta, rho, sigma, omega):
+def _predicted_labor_share(capital, g, n, s, delta, rho, omega):
     """Model predicted share of income going to labor."""
     if abs(rho) <= 1e-3:
         labor_share = omega
@@ -13,18 +15,30 @@ def predicted_labor_share(capital, g, n, s, delta, rho, sigma, omega):
 
     return labor_share
 
-def residual(capital, ctry, params):
+def _residual(capital, ctry, g, n, s, delta, rho, omega):
     """Difference between actual and model predicted labor share."""
     actual_labor_share = pwt_data.major_xs(ctry)['labsh']
-    return actual_labor_share - predicted_labor_share(capital, **params)
+    predicted_labor_share = _predicted_labor_share(capital, g, n, s, delta, rho, omega)
+    return actual_labor_share - predicted_labor_share
+
+def individual_log_likelihood(capital, ctry, g, n, s, delta, rho, sigma, omega):
+    """Individual model log likelihood."""
+    # residual is assumed to be drawn from Gaussian with mean zero!
+    rv = stats.norm(0, sigma)
+
+    # compute the individual log-likelihood
+    residual = _residual(capital, ctry, g, n, s, delta, rho, omega)
+    individual_likelihood = rv.pdf(residual)
+    return np.log(individual_likelihood)
+
 
 if __name__ == '__main__':
     
-    test_params = {'n':0.02, 'g':0.02, 's':0.15, 'delta':0.04, 'rho':1.5,
-                    'sigma':0.01, 'omega':0.33}
+    # ordering is g, n, s, delta, rho, sigma, omega
+    test_params = np.array([0.02, 0.02, 0.15, 0.04, 1.5, 0.01, 0.33])
 
     ctry = 'USA'
     N = pwt_data.major_xs(ctry)['labsh'].size
     test_capital = np.ones(N)
 
-    print residual(test_capital, ctry, test_params)
+    print individual_log_likelihood(test_capital, ctry, *test_params)
