64f906a4dd5d90c4bfe0cc1d0c94afb15dcd2a77
David R. Pugh <drobert.pugh@gmail.com>
2014-06-30 10:45:55 +0100

    Added a residual function for computing the difference between actual and model predicted labor shares.

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pwt
+
+# load the Penn World Tables data
+pwt_data = pwt.load_pwt_data()
+
 def predicted_labor_share(capital, g, n, s, delta, rho, sigma, omega):
     """Model predicted share of income going to labor."""
     if abs(rho) <= 1e-3:
@@ -7,8 +13,18 @@ def predicted_labor_share(capital, g, n, s, delta, rho, sigma, omega):
 
     return labor_share
 
+def residual(capital, ctry, params):
+    """Difference between actual and model predicted labor share."""
+    actual_labor_share = pwt_data.major_xs(ctry)['labsh']
+    return actual_labor_share - predicted_labor_share(capital, **params)
+
 if __name__ == '__main__':
+    
     test_params = {'n':0.02, 'g':0.02, 's':0.15, 'delta':0.04, 'rho':1.5,
                     'sigma':0.01, 'omega':0.33}
 
-    print predicted_labor_share(10.0, **test_params)
+    ctry = 'USA'
+    N = pwt_data.major_xs(ctry)['labsh'].size
+    test_capital = np.ones(N)
+
+    print residual(test_capital, ctry, test_params)
