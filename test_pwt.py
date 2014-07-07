77da53a105867cfc5c133dde29482cf5d33dce2e
David R. Pugh <drobert.pugh@gmail.com>
2014-07-07 12:09:25 -0500

    Only need to test the public function!

--- test_pwt.py
+++ test_pwt.py
@@ -1,21 +1,6 @@
 """Unit testing framework for pwt.py module."""
 
 
-def test_get_dep_rates_data(base_url, version):
-    """Test _get_dep_rates_data function."""
-    raise NotImplementedError
-
-
-def test_get_pwt_data(base_url, version):
-    """Test _get_pwt_data function."""
-    raise NotImplementedError
-
-
-def test_download_pwt_data(base_url, version):
-    """Test _download_pwt_data function."""
-    raise NotImplementedError
-
-
 def test_load_pwt_data():
     """Test load_pwt_data function."""
     raise NotImplementedError
