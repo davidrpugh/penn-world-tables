bac2cce0ec55ce3928eec916c246df4dec016a61
David R. Pugh <drobert.pugh@gmail.com>
2014-07-29 13:16:43 +0100

    Added an import of pandas.

--- test_pwt.py
+++ test_pwt.py
@@ -2,6 +2,8 @@
 import os
 import unittest
 
+import pandas as pd
+
 import pwt
 
 
@@ -52,14 +54,8 @@ class TestPWTDownload(unittest.TestCase):
         mesg = "Download of " + tmp_file + "file failed!"
         assert os.path.isfile(tmp_file), mesg
 
-# @with_setup(setup_func, teardown_func)
-# def test_load_pwt_data(version=80):
-#     """Test load_pwt_data function."""
-#     data = pwt.load_pwt_data(base_url='http://www.rug.nl/research/ggdc/data/pwt/',
-#                              version=version)
-
-#     # assert that files have been downloaded
-#     mesg = "Download of 'depreciation_rates.dta' file failed!"
-#     assert os.path.isfile('depreciation_rates.dta'), mesg
-#     mesg = "Download of " + 'pwt' + str(version) + '.dta' + "file failed!"
-#     assert os.path.isfile('pwt' + str(version) + '.dta'), mesg
+    def test_load_pwt_data(self):
+        """Test that PWT data has been loaded correctly."""
+        data = pwt.load_pwt_data(base_url=self.base_url,
+                                 version=self.version)
+        self.assertIsInstance(data, pd.Panel)
