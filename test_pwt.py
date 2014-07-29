e0c8a7f52d26085bc248f3c07995a1dd07d6b5eb
David R. Pugh <drobert.pugh@gmail.com>
2014-07-29 13:09:36 +0100

    Added a test function for _download_pwt_data.

--- test_pwt.py
+++ test_pwt.py
@@ -40,6 +40,18 @@ class TestPWTDownload(unittest.TestCase):
         mesg = "Download of " + tmp_file + "file failed!"
         assert os.path.isfile(tmp_file), mesg
 
+    def test__download_pwt_data(self):
+        """Test download of combined data set."""
+        pwt._download_pwt_data(self.base_url, self.version)
+
+        # assert that files have been downloaded
+        mesg = "Download of 'depreciation_rates.dta' file failed!"
+        assert os.path.isfile('depreciation_rates.dta'), mesg
+
+        tmp_file = 'pwt' + str(self.version) + '.dta'
+        mesg = "Download of " + tmp_file + "file failed!"
+        assert os.path.isfile(tmp_file), mesg
+
 # @with_setup(setup_func, teardown_func)
 # def test_load_pwt_data(version=80):
 #     """Test load_pwt_data function."""
