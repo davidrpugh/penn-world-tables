aa63ce22f3f294e4ee9ebf523f618cc605708bc7
David R. Pugh <drobert.pugh@gmail.com>
2014-07-30 15:27:35 +0100

    Added some more classifiers.

--- pyPWT/setup.py
+++ pyPWT/setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pypwt',
+      packages=find_packages(exclude=['test']),
       version='0.1.0',
       description='Python package for generating the Penn World Tables data set.',
       author='David R. Pugh',
@@ -9,7 +10,10 @@ setup(name='pypwt',
       license='LICENSE.txt',
       install_requires=['pandas'],
       classifiers=['Development Status :: 1 - Planning',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: Science/Research',
                    'License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
+                   'Programming Language :: Python',
                    ]
       )
