753a20458a492f3ed4c77377cc09157047dae3bb
David R. Pugh <drobert.pugh@gmail.com>
2014-10-31 06:43:50 -0400

    Fixed init file.

---
+++ pyPWT/pypwt/__init__.py
@@ -0,0 +1,12 @@
+"""
+PyPWT imports:
+    Objects imported here will live in the `pypwt` namespace.
+
+@author : David R. Pugh
+@date : 2014-10-31
+
+"""
+
+__all__ = ["load_pwt_data"]
+
+from . io import load_pwt_data
